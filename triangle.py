a583f7e0acdafc06805235a0e4352440be45125e
danikdanik2013 <danikdanik20166@gmail.com>
2018-05-23 15:24:46 +0300

    v4

--- triangle.py
+++ triangle.py
@@ -3,80 +3,80 @@ from math import acos, degrees
 
 
 while True:
-	a = float(input('Enter first side: '))
 	try:
+		a = float(input('Enter first side: '))
 		float(a)
 		break
 	except ValueError:
 		print("Enter float number!")
 		pass
 while True:		
-	b = float(input('Enter second side: '))
 	try:
+		b = float(input('Enter second side: '))
 		float(b)
 		break
 	except ValueError:
 		print("Enter float number!")
 		pass
 while True:
-	c = float(input('Enter third side: '))
 	try:
+		c = float(input('Enter third side: '))
 		float(c)
 		break
 	except ValueError:
 		print("Enter float number!")
 		pass
 
+a = int(a)
+b = int(b)
+c = int(c)
+
+fi_3 = acos((c ** 2 - a ** 2 - b ** 2)/(-2 * a * b))
+fi_3 = degrees(fi_3)
+fi_2 = acos((a ** 2 - b ** 2 - c ** 2)/(-2 * b * c))
+fi_2 = degrees(fi_2)
+fi_1 = 180 - fi_3 - fi_2
 while True:
 	if a < 0 or b < 0 or c < 0:
 		print("uncorrect")
 		break
-	if a > b:
+	elif a > b:
 		if (a + b) < c or (a - b) > c or (a + b) == c:
 			print("ucorrect")
 			break
-	if b > a:
+	elif b > a:
 		if (b + a) < c or (b - a) > c or (b + a) == c :
 			print("ucorrect")
 			break
-	if a == b:
+	elif a == b:
 		if (a + b) < c or (b + a) < c or (a - b) > c or (b - a) > c or (a + b) == c:
 			print("uncorrect")
 			break
-	if a > c:
+	elif a > c:
 		if (a + c) < b or (a - c) > b or (a + c) == b:
 			print("ucorrect")
 			break
-	if c > a:
+	elif c > a:
 		if (c + a) < b or (c - a) > b or (c + a) == b:
 			print("uncorrect")
 			break
-	if a == c:
+	elif a == c:
 		if (a + c) < c or (c + c) < c or (a - c) > b or (c - a) > b or (c + a) == b:
 			print("uncorrect")
 			break
-	if b > c:
+	elif b > c:
 		if (b + c) < a or (b - c) > a or (b + c) == a :
 			print("uncorrect")
 			break
-	if c > b:	
+	elif c > b:	
 		if (c + b) < a or (c - b) > a or (c + b) == a:
 			print("uncorrect")
 			break
-	if b == c:
+	elif b == c:
 		if (b + c) < a or (c + b) < a or (c - b) > a or (b - c) > a or (b + c ) == a:
 			print("uncorrect")
 			break
 
-	a = int(a)
-	b = int(b)
-	c = int(c)
-
-	fi_3 = acos((c ** 2 - a ** 2 - b ** 2)/(-2 * a * b))
-	fi_3 = degrees(fi_3)
-	fi_2 = acos((a ** 2 - b ** 2 - c ** 2)/(-2 * b * c))
-	fi_2 = degrees(fi_2)
-	fi_1 = 180 - fi_3 - fi_2
 
 	if a == b == c:
 		print("That Equal triangle")
@@ -99,4 +99,3 @@ while True:
 		print("that Versatile triangle")
 		break
 		pass
-#
